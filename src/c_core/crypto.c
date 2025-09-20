702c299c19a6ad5408ca501f5344909975d6c091
Gerardo <therambien@hotmail.com>
2025-09-20 00:04:18 -0600

    fix(crypto) correct ciphertext length calculation in decryption

--- src/c_core/crypto.c
+++ src/c_core/crypto.c
@@ -10,26 +10,27 @@ typedef struct  {
 }BytesBuffer;
 
 
-BytesBuffer encrypt_messenge(const unsigned char* messenge, size_t messenge_len, const unsigned char* key){
+BytesBuffer encrypt_message(const unsigned char* message, size_t message_len, const unsigned char* key){
+  printf("[C-DEBUG] Entrando a encrypt_message. Longitud del mensaje: %zu\n", message_len);
   if(sodium_init() < 0) {
     return (BytesBuffer){NULL, 0};
   }
 
-  unsigned long long ciphertext_len = crypto_secretbox_MACBYTES + messenge_len;
+  unsigned long long ciphertext_len = crypto_secretbox_MACBYTES + message_len;
   unsigned char* ciphertext = malloc(ciphertext_len);
   if (ciphertext == NULL){
     return (BytesBuffer){NULL, 0};
   }
-
+  
   unsigned char nonce[crypto_secretbox_NONCEBYTES];
   randombytes_buf(nonce, sizeof(nonce));
 
-  crypto_secretbox_easy(ciphertext, messenge, messenge_len, nonce, key);
+  crypto_secretbox_easy(ciphertext, message, message_len, nonce, key);
 
   size_t total_len = sizeof(nonce) + ciphertext_len;
   char* full_payload = malloc(total_len);
   if (full_payload == NULL){
-    free(full_payload);
+    free(ciphertext);
     return (BytesBuffer){NULL, 0};
   }
 
@@ -38,10 +39,11 @@ BytesBuffer encrypt_messenge(const unsigned char* messenge, size_t messenge_len,
   
   free(ciphertext);
 
+  printf("[C-DEBUG] Saliendo de encrypt_message. Longitud del payload: %zu\n", total_len);
   return (BytesBuffer){full_payload, total_len};
 }
 
-BytesBuffer decrypt_messenge(const unsigned char* full_payload, size_t payload_len, const unsigned char* key){
+BytesBuffer decrypt_message(const unsigned char* full_payload, size_t payload_len, const unsigned char* key){
   if (sodium_init() < 0) {
     return (BytesBuffer){NULL, 0};
   }
@@ -52,7 +54,7 @@ BytesBuffer decrypt_messenge(const unsigned char* full_payload, size_t payload_l
 
   const unsigned char* nonce = full_payload;
   const unsigned char* ciphertext = full_payload + crypto_secretbox_NONCEBYTES;
-  size_t ciphertext_len = payload_len - crypto_secretbox_MACBYTES;
+  size_t ciphertext_len = payload_len - crypto_secretbox_NONCEBYTES;
 
   size_t decrypted_len = ciphertext_len - crypto_secretbox_MACBYTES;
   unsigned char* decrypted = malloc(decrypted_len);
@@ -67,6 +69,10 @@ BytesBuffer decrypt_messenge(const unsigned char* full_payload, size_t payload_l
   }
 
   return (BytesBuffer){(char*)decrypted, decrypted_len};
+}
+
+size_t get_key_bytes(void) {
+  return crypto_secretbox_KEYBYTES;
 }
 
 void free_buffer(BytesBuffer buffer) {
